498aa6f3ed9166451f71ebffb25d1e6ed41ddee1
Lucas Cividini <lucaspagnussat5@gmail.com>
2023-04-21 21:46:34 -0300

    Exception created and defensive programming applicated

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer rows, Integer colums) {
+        if(rows < 1 || colums < 1){
+            throw new BoardException("Error creating board: there must be at least one row and one column to start the game");
+        }
         this.rows = rows;
         this.colums = colums;
         pieces = new Piece[rows][colums];
@@ -15,27 +18,43 @@ public class Board {
     public Integer getRows() {
         return rows;
     }
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
     public Integer getColums() {
         return colums;
     }
-    public void setColums(Integer colums) {
-        this.colums = colums;
-    }
-
-    public Piece pieace(int row, int column){
+    public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Position is not in the board of the game!");
+        }
         return pieces[row][column];
     }
-
-    public Piece pieace(Position position){
+    public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position is not in the board of the game!");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position" + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >=0 && column < colums;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position is not in the board of the game!");
+        }
+        return piece(position) != null;
     }
 
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -17,7 +17,7 @@ public class ChessMatch {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColums()];
         for(int i =0; i < board.getRows(); i++){
             for(int j =0; j < board.getColums(); j++){
-                mat[i][j] = (ChessPiece) board.pieace(i, j);
+                mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
